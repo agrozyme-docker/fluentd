83c0d40b53dcecf54d82ae3144b708c5db8bbd66
agrozyme <agrozyme@gmail.com>
2018-12-30 01:09:19 +0800

    update

--- source/usr/local/bin/agrozyme.fluentd.command.sh
+++ source/usr/local/bin/agrozyme.fluentd.command.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 function main() {
   agrozyme.alpine.function.sh change_core
-  exec fluentd --user core --group core
+  # exec fluentd --user core --group core
+  exec fluentd
 }
 
 main "$@"
