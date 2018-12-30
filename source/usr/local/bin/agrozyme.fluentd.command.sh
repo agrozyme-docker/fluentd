60320ac2caa3f50948dc7ddf028e8a33fa97b040
agrozyme <agrozyme@gmail.com>
2018-12-30 09:04:06 +0800

    update

--- source/usr/local/bin/agrozyme.fluentd.command.sh
+++ source/usr/local/bin/agrozyme.fluentd.command.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-function change_core() {
-  local old_uid=500
-  local old_gid=500
-  local new_uid=${DOCKER_UID:-${old_uid}}
-  local new_gid=${DOCKER_GID:-${old_gid}}
-  groupmod -g "${new_gid}" core
-  usermod -u "${new_uid}" core
+function create_core() {
+  groupadd -rg 500 core
+  useradd -Nr -u 500 -g core -s /bin/sh -c core core
 }
 
 function main() {
-  change_core
-  # agrozyme.alpine.function.sh change_core
+  create_core
+  agrozyme.alpine.function.sh change_core
   chown -R core:core /var/log/fluent
   exec fluentd --user core --group core
 }
